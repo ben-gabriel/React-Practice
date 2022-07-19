1f0c3c59d4eed6567a3486087429fd03a770902a
Sergio <ben.gabriel.w@gmail.com>
2022-07-19 16:15:55 -0300

    test 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = ()=>{
 
         <div className="App">
             <header>
-                <h1>React Todo List change test</h1>
+                <h1>React Todo List change test 2</h1>
             </header>
 
             <Form />
