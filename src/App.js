2be8ab1bc0697c1a3f87a6750541dd3eb60d90f1
Sergio <ben.gabriel.w@gmail.com>
2022-07-23 17:08:52 -0300

    add delete todo item handler

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ const App = ()=>{
 
             <TodoList 
                 todos={todos}
+                setTodos={setTodos}
             />
 
         </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Todo from './Todo'
 
-const TodoList = ({todos})=>{
+const TodoList = ({todos, setTodos})=>{
     
     return(
         <div className="todo-container">
@@ -9,7 +9,10 @@ const TodoList = ({todos})=>{
                 {todos.map((todo)=>(
                     <Todo 
                         text={todo.text} 
-                        id={todo.id}
+                        key={todo.id}
+                        todos={todos}
+                        todo={todo}
+                        setTodos={setTodos}
                     />
                 ))}
 
