0bdc3a1a5e5b00dddc4c01bad4da823a52e930b9
Sergio <ben.gabriel.w@gmail.com>
2022-07-24 01:52:42 -0300

    add complete handler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,12 +6,35 @@ const Todo = ({text, todos, todo, setTodos})=>{
         setTodos(todos.filter((element) => element.id !== todo.id))
     }
 
+    const completeHandler = ()=>{
+        // let arraymap = todos.map((el)=> {
+        //     if(el.id === todo.id){ 
+        //         return {...el, completed : !el.completed};
+        //     }
+        //     else{
+        //         return el;
+        //     }
+        // });
+       
+        // setTodos(arraymap);
+
+        //-----------------------------
+        // let arraymap = todos.map((el)=> 
+        //     (el.id === todo.id) ? {...el, completed : !el.completed} : el
+        // );
+        
+        // console.log(arraymap)
+        // setTodos(arraymap);
+
+        //-----------------------------
+        setTodos(todos.map((el)=> (el.id === todo.id) ? {...el, completed : !el.completed} : el));
+    }
 
     return (
-        <div className="todo">
+        <div className={`todo ${todo.completed ? 'completed': ''}`} >
             <li className="todo-item">{text}</li>
             
-            <button className="complete-btn">
+            <button onClick={completeHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
             
